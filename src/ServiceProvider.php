876441fb6d687919205b60658881950e2197761f
kevin olson <acidjazz@gmail.com>
2016-07-21 19:28:17 -0700

    initial check-in

---
+++ src/ServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace larpug;
+
+use Illuminate\View\ViewServiceProvider;
+use InvalidArgumentException;
+
+class ServiceProvider extends ViewServiceProvider
+{
+
+  public function register() {
+
+  }
+
+  public function boot() {
+
+  }
+
+  public function isLumen() {
+    return strpos($this->app->version, 'Lumen') !== false;
+  }
+  
+  public function registerExtension() {
+    $this->app['view']->addExtension(
+      $this->app['pug.extension'],
+      'pug',
+      function() {
+        return $this->app['pug.engine'];
+      }
+    );
+  }
+
+}
