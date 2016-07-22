4f132cce88c9dcf4705628fcfa49f229a4506d38
kevin olson <acidjazz@gmail.com>
2016-07-22 01:04:40 -0700

    succesful pug compilation via laravel

---
+++ node/larpug.js
@@ -0,0 +1,45 @@
+
+/**
+ * local pug delivery via localhost web port 4200
+ */
+
+var fs = require('fs');
+var pug = require('pug');
+var http = require('http');
+var path = require('path');
+
+http.createServer(function(req, res) {
+
+  if (req.method != 'POST') {
+    res.writeHead(500, {'Content-Type': 'text/json'});
+    res.end(JSON.stringify({'error': 'post required'}));
+    return true;
+  }
+
+  var body = '';
+
+  req.on('data', function(data) {
+    body += data;
+  });
+
+  req.on('end', function() {
+
+    var post = JSON.parse(body);
+
+    if (post.module == 'pug') {
+
+      pug.renderFile(post.options.file, post.data, function(error, output) {
+        if (error) {
+          res.writeHead(500, {'Content-Type': 'text/html'});
+          res.end(error.message); 
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(output);
+        }
+      });
+
+    }
+
+  });
+
+}).listen(4242);
--- src/Pug.php
+++ src/Pug.php
@@ -2,17 +2,36 @@
 
 namespace larpug;
 use Illuminate\View\Engines\EngineInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Pug implements EngineInterface  {
 
   public function __construct() {
+
   }
 
   public function get($path, array $data=[]) {
 
     \Debugbar::info($data);
 
-    return file_get_contents($path);
+    $result = $this->compile($path, $data);
+
+    return $result['data'];
+
+  }
+
+  public function compile($path, $data) {
+
+    $result = Node::post('pug', 'http://localhost:4242/', ['file' => $path], $data);
+
+    if ($result['status'] == 500) {
+      trigger_error('Compilation Error: '.$result['data']);
+      return false;
+
+    }
+
+    return $result;
 
   }
 
