bed75b0af384a843b938f24fe06a0a58a110c760
kevin olson <acidjazz@gmail.com>
2016-07-24 00:18:23 -0700

    went singletone, supported larjectus

--- src/Pug.php
+++ src/Pug.php
@@ -2,27 +2,30 @@
 
 namespace larpug;
 use Illuminate\View\Engines\EngineInterface;
+use Illuminate\View\Factory;
+use Illuminate\Contracts\Container\Container; 
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Pug implements EngineInterface  {
+class Pug extends Factory implements EngineInterface {
 
-  public function __construct() {
+  protected $app;
 
+  public function __construct(Container $app = null) {
+    $this->app = $app;
   }
 
   public function get($path, array $data=[]) {
-
     $result = $this->compile($path, $data);
-
     return $result['data'];
-
   }
 
   public function compile($path, $data) {
 
     $data['pretty'] = true;
     $data['self'] = true;
+
+    $data = $this->mergeObjectus($data);
 
     $result = Node::post('pug', 'http://localhost:4242/',['file' => $path], $data);
 
@@ -34,6 +37,15 @@ class Pug implements EngineInterface  {
 
     return $result;
 
+  }
+
+  public function mergeObjectus($data) {
+
+    if (class_exists('\larjectus\Objectus')) {
+      $data['config'] = \larjectus\Objectus::get($this->app->basePath() . '/config/');
+    }
+
+    return $data;
   }
 
 }
