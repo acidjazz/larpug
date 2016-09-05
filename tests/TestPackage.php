09914a7af1ad166c0e4aafc6539d20863fdb925d
kevin olson <acidjazz@gmail.com>
2016-09-05 00:48:48 -0700

    actual view rendering test

--- tests/TestPackage.php
+++ tests/TestPackage.php
@@ -5,7 +5,6 @@ namespace Orchestra\Testbench\TestCase;
 class TestPackage extends \Orchestra\Testbench\TestCase {
 
   public function testConfig() {
-
     $test = new TestApp();
     $app = $test->createApplication();
     $this->assertEquals(config('app.env'), 'testing');
@@ -15,6 +14,30 @@ class TestPackage extends \Orchestra\Testbench\TestCase {
     $test = new TestApp();
     $app = $test->createApplication();
     $this->assertContains('Larpug\ServiceProvider',config('app.providers'));
+  }
+
+  public function testView() {
+
+    $result = '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>\'this is a title\'</title>
+  </head>
+  <body>
+    <div class="test">test rendering</div>
+  </body>
+</html>';
+
+    $test = new TestApp();
+    $app = $test->createApplication();
+
+
+    echo __DIR__ . './resources/views';
+    view()->addLocation(__DIR__ . '/resources/views');
+    $view = view('pages.test1')->render();
+    $this->assertEquals($view, $result);
+
+
   }
 
 }
